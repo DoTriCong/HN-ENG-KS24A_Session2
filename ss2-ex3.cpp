ec3b131637a880f00be3c3c6f40f336a45e1b08a
DoTriCong <tricong5526@gmail.com>
2024-11-21 01:11:47 +0700

    Update ss2-ex3.cpp

--- ss2-ex3.cpp
+++ ss2-ex3.cpp
@@ -2,13 +2,13 @@
 
 int main() {
     //Khai bao va khoi tao hai bien kieu int
-    int a = 5;  //Bien a duoc khoi tao voi gi· tri 5
-    int b = 10; //Bien b duoc khoi tao voi gi· tri 10
+    int a = 5;  //Bien a duoc khoi tao voi gi√° tri 5
+    int b = 10; //Bien b duoc khoi tao voi gi√° tri 10
 
     //Khai bao cac bien kieu int de luu ket qua tinh toan
-    int difference = a - b;  //TÌnh hieu caa a v‡ b
-    int product = a * b;     //TÌnh tich cua a v‡ b
-    float quotient = (float)a / b;  // TÌnh thuong cua a v‡ b (chuyen a th‡nh float de cÛ ket qua chinh xac)
+    int difference = a - b;  //T√≠nh hieu cua a v√† b
+    int product = a * b;     //T√≠nh tich cua a v√† b
+    float quotient = (float)a / b;  // T√≠nh thuong cua a v√† b (chuyen a th√†nh float de c√≥ ket qua chinh xac)
 
     //In ket qua ra man hinh
     printf("Hieu cua %d va %d la %d\n", a, b, difference);    //In ra hieu
